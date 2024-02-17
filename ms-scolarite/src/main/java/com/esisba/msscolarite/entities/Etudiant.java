c334cd9fbb31a976a288bf847d7543ff97fc3e4d
Ilyas Benhammadi <i.benhammadi@esi-sba.dz>
2024-02-18 00:54:12 +0100

    init

---
+++ ms-scolarite/src/main/java/com/esisba/msscolarite/entities/Etudiant.java
@@ -0,0 +1,31 @@
+package com.esisba.msscolarite.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Etudiant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateN;
+
+    private String promo;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateInscription;
+
+    @ManyToOne
+    private Etablissement etablissement;
+}
